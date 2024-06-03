c6f7664cb9109d7b28db988c60dabe2e45060886
TranNamCHY <135244822+TranNamCHY@users.noreply.github.com>
2024-06-03 16:54:09 +0700

    delete uncessary file

--- Reg.vhd
+++ Reg.vhd
@@ -5,7 +5,7 @@ ENTITY Reg IS
  GENERIC ( DATA_WIDTH : Integer := 8);
  PORT (
 	Reset,Clk : IN STD_LOGIC;
-	En : IN STD_LOGIC;
+	Enable : IN STD_LOGIC;
 	D : IN STD_LOGIC_VECTOR (DATA_WIDTH - 1 downto 0);
  	Q : OUT STD_LOGIC_VECTOR (DATA_WIDTH - 1 downto 0)
  );
@@ -18,7 +18,7 @@ BEGIN
   IF(Reset= '1') THEN
    Q <= (OTHERS => '0');
   ELSIF(Clk'EVENT and Clk = '1') THEN
-   IF(EN = '1') THEN
+   IF(Enable = '1') THEN
     Q <= D;
    END IF;
   END IF;
